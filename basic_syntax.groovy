6c519b4316f5ad504d6f50b82fe558c827b35c1a
alexanderboyuanzhang <alexander.boyuan.zhang@gmail.com>
2019-11-27 21:59:33 +0100

    data_types, basic_syntax

---
+++ basic_syntax.groovy
@@ -0,0 +1,10 @@
+class Example {
+   static void main(String[] args) {
+      // Using a simple println statement to print output to the console
+      println('Hello World');
+
+      def a = 5;
+
+      println(a);
+   }
+}
